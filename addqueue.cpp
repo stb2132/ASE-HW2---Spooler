6f685e5246b7079405d6293c565d7b7af768ad05
Spencer Thomas Brown <stb2132@columbia.edu>
2015-11-09 11:56:16 -0500

    Begin creation of addqueue command

---
+++ addqueue.cpp
@@ -0,0 +1,31 @@
+#include <fstream>
+#include <iostream>
+#include "addqueue.h"
+
+
+int copyfile(std::string src, std::string dest){
+
+
+    return 0;
+}
+
+int validatefile(std::string name){
+
+
+}
+
+
+
+
+int main(int argc, char *argv[]){
+
+    if(argc < 2){
+      std::cout << "Please run the command as '$ addqueue <arguments>'." << std::endl;
+
+    } else{
+      std::cout << "Hello there" << std::endl;
+    }
+
+    return 0;
+
+}
