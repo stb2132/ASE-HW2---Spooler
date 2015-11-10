04906d68c0e70a4764fd19ecbc2a8e1212475b8d
tinyvm <tinyvm@sec-vm07.cs.columbia.edu>
2015-11-09 23:15:54 -0500

    Finish date printing. Split directory reading function

--- queuehelper.h
+++ queuehelper.h
@@ -1,15 +1,41 @@
 #include <boost/filesystem.hpp>
-#include <boost/range/iterator_range.hpp>
 #include <string>
 #include <iostream>
 #include <ctime>
 #include <map>
 
-namespace fs = boost::filesystem3;
+#define TIME_FORMAT "%Y-%m-%d_%H:%M:%S"
+
+namespace fs = boost::filesystem;
+typedef std::multimap<std::time_t, fs::path> result_map;
+
+std::string format_time(std::string format, time_t time){
+    struct tm *info;
+    info = localtime(&time);
+    std::string buf;
+
+    buf.resize(20);
+    int len = strftime(&buf[0], buf.size(), format.c_str(), info);
+    while(len == 0){
+        buf.resize(buf.size() * 2);
+        int len = strftime(&buf[0], buf.size(), format.c_str(), info);
+    }
+    buf.resize(len);
+    return buf;
+}
+
+//This gets the path/time of creation for a given directory
+void fill_directory_map(fs::path dir_path, result_map &map){
+    fs::directory_iterator end_iter;
+
+    for(fs::directory_iterator dir_iter(dir_path); dir_iter != end_iter ; ++dir_iter){
+        if(fs::is_regular_file(dir_iter->status()) ){
+            map.insert(result_map::value_type(fs::last_write_time(dir_iter->path()), *dir_iter)); 
+        }
+    }
+}
 
 int list_dir(std::string src){
-    typedef std::multimap<std::time_t, fs::path> result_map;
-    fs::directory_iterator end_iter;
     result_map map;
     fs::path p (src);
     std::time_t last_write;
@@ -17,18 +43,12 @@ int list_dir(std::string src){
     try{
         if(exists(p)){
             if(is_directory(p)){
-                //for(auto& entry : boost::make_iterator_range(directory_iterator(p), {} )){
-                for(fs::directory_iterator dir_iter(p); dir_iter != end_iter ; ++dir_iter){
-                    if(fs::is_regular_file(dir_iter->status()) ){
-                        map.insert(result_map::value_type(fs::last_write_time(dir_iter->path()), *dir_iter)); 
-                    }
-                }
+                fill_directory_map(p, map);
+                
                 for(result_map::iterator it = map.begin(); it != map.end(); ++it){
-                    std::cout << it->first << " " << it->second << std::endl;
+                    std::cout << format_time(TIME_FORMAT, it->first) << " " << it->second << std::endl;
                 }
-            } else {
-                std::cout << "noooo" << std::endl;
-            }
+	    }
         }
 
     } catch(const fs::filesystem_error& ex){
